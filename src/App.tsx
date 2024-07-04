170a62897dd566c09cccc318032d3e4d2749293d
IzumiSy <beetle-noise@gmx.com>
2024-07-04 16:18:01 +0900

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,68 @@
-import { FormLayout } from "@formily/antd";
 import { createForm } from "@formily/core";
-import { FormProvider } from "@formily/react";
+import { FormProvider, createSchemaField } from "@formily/react";
+import {
+  Input,
+  Select,
+  FormItem,
+  FormLayout,
+  Submit,
+  FormButtonGroup,
+} from "@formily/antd";
+
+const SchemaField = createSchemaField({
+  components: {
+    Input,
+    Select,
+    FormItem,
+    FormLayout,
+  },
+});
+
+const schema = {
+  type: "object",
+  properties: {
+    layout: {
+      type: "void",
+      "x-component": "FormLayout",
+      "x-component-props": {
+        labelCol: 6,
+        wrapperCol: 10,
+        layout: "vertical",
+      },
+      properties: {
+        input: {
+          type: "string",
+          title: "input box",
+          required: true,
+          "x-decorator": "FormItem",
+          "x-decorator-props": {
+            tooltip: <div>123</div>,
+          },
+          "x-component": "Input",
+        },
+        select: {
+          type: "string",
+          title: "Select box",
+          required: true,
+          "x-decorator": "FormItem",
+          "x-component": "Select",
+        },
+      },
+    },
+  },
+};
+
+const form = createForm();
 
 function App() {
-  const form = createForm();
-
   return (
-    <>
+    <FormProvider form={form}>
       <h1>Vite + React</h1>
-      <FormProvider form={form}>
-        <FormLayout></FormLayout>
-      </FormProvider>
-    </>
+      <SchemaField schema={schema} />
+      <FormButtonGroup>
+        <Submit onSubmit={console.log}>submit</Submit>
+      </FormButtonGroup>
+    </FormProvider>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~antd": path.resolve(__dirname, "node_modules/antd"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
 });
